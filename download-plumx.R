449da5578883fae198b861086bda7318c66e55e9
Camilla Hertil Lindelöw <camilla.hertil.lindelow@sh.se>
2016-12-14 11:34:10 +0000

    nedladdning av PlumX-data till json-fil månadsvis

---
+++ download-plumx.R
@@ -0,0 +1,18 @@
+# Ladda ner PlumX-data varje månad
+source('/home/shub/assets/cred.R')
+key = plu.mx.auth
+
+# Om vi kör som shub så skriver vi till vårt gemensamma bibliotek
+if (Sys.info()['user'] == 'shub') {
+  px.filename = "/home/shub/assets/plumx.JSON"  
+} else {
+  # Annars till lokal plats
+  px.filename = "plumx.JSON"
+}
+
+if (is.na(file.info(px.filename)$mtime) ||
+    file.info(px.filename)$mtime < Sys.time()-(60*60*24*30)) {
+  library(httr)
+  r <- GET("https://api.plu.mx/g/sh-se/_artifacts", query = list(auth=key))
+  writeBin(content(r, 'raw'), px.filename)
+}

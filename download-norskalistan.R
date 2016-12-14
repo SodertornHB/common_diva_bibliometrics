cacd15c3b6718864d7ba7e5463936a049b85ead0
Jonas Oberg <jonas.oberg@sh.se>
2016-12-14 08:27:58 +0000

    Added utility to automatically download norska listan on a monthly basis.

---
+++ download-norskalistan.R
@@ -0,0 +1,13 @@
+# Ladda ner Norska listan varje månad
+source('/home/shub/assets/cred.R')
+nsd.filename = "/home/shub/assets/nsd.list-approved.csv"
+if (is.na(file.info(nsd.filename)$mtime) ||
+    file.info(nsd.filename)$mtime < Sys.time()-(60*60*24*30)) {
+  library(httr)
+  r <- POST("https://dbh.nsd.uib.no/publiseringskanaler/BrukerLoggpaSjekk.action",
+            body = list(skjemaPassord = nsd.password,
+                        BrukerLoggpaSjekk_0 = 'Logg+inn',
+                        skjemaEpost = nsd.username))
+  r <- GET("https://dbh.nsd.uib.no/publiseringskanaler/erihplus/periodical/listApprovedAsCsv")
+  writeBin(content(r, 'raw'), nsd.filename)
+}

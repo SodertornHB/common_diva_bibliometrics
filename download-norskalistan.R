7f0f1d699f0d145adf329a8ca55de36cb0e842b4
Jonas Oberg <jonas.oberg@sh.se>
2016-12-14 10:49:37 +0000

    Liten förändring för att sätta filnamn korrekt

--- download-norskalistan.R
+++ download-norskalistan.R
@@ -1,6 +1,14 @@
 # Ladda ner Norska listan varje månad
 source('/home/shub/assets/cred.R')
-nsd.filename = "/home/shub/assets/nsd.list-approved.csv"
+
+# Om vi kör som shub så skriver vi till vårt gemensamma bibliotek
+if (Sys.info()['user'] == 'shub') {
+  nsd.filename = "/home/shub/assets/nsd.list-approved.csv"  
+} else {
+  # Annars till lokal plats
+  nsd.filename = "nsd.list-approved.csv"
+}
+
 if (is.na(file.info(nsd.filename)$mtime) ||
     file.info(nsd.filename)$mtime < Sys.time()-(60*60*24*30)) {
   library(httr)

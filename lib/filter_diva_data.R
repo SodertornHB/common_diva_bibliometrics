bf8781b5e49fad130532de386d6aa5446a720b5b
Camilla Hertil Lindelöw <camilla.hertil.lindelow@sh.se>
2017-08-17 11:02:34 +0000

    fyllt på med filter-funktioner för divadata

--- lib/filter_diva_data.R
+++ lib/filter_diva_data.R
@@ -1,21 +1,116 @@
+#
+#
+#
 # filter-funktioner
 #CHL 170816
-#filter-funktionerna filtrerar diva-tibbles i shub-assets
+#filter-funktionerna filtrerar diva-data från assets: kom ihåg att välja rätt uval därifrån
+#library(tidyverse)
+#
+#
+#
 
-#filter_ref_ovr_vet: övrigt vet och refgr
-filter_ref_ovr_vet <- function(x) {
-  library(tidyverse)
-  a = x %>%
+# PublicationType ---------------------------------------------------------
+
+#filter_publtyp_art_bok_kap_konf: tar fram endast art (art + forsknöversikt),
+#bok, kap, och konfbidrag (med abstract etc om inte publsubtype-filter tillämpas)
+
+filter_publtyp_art_bok_kap_konf <- function(divadata) {
+  a = divadata %>%
+    filter(PublicationType %in% c("Artikel, forskningsöversikt", "Artikel i tidskrift",
+                                  "Bok", "Kapitel i bok, del av antologi", "Konferensbidrag"))
+  return(a)
+}
+
+
+#filter_publtyp_avh: tar fram endast avhandlingar (monografier och sammanläggnings)
+filter_publtyp_avh <- function(divadata) {
+  a = divadata %>%
+    filter(PublicationType %in% c("Doktorsavhandling, monografi", "Doktorsavhandling, sammanläggning"))
+  return(a)
+}
+
+
+# ContentType -------------------------------------------------------------
+
+#filter contenttype: obs att studentuppsats har Content Type NA
+
+#filter_content_ovrig_no: tar bort kategori "Övrig (populärvetenskap, debatt, mm)"
+filter_content_ovrig_no <- function(divadata) {
+  a = divadata %>%
     filter(ContentType != "Övrig (populärvetenskap, debatt, mm)")
   return(a)
 }
 
-filter_konf_publ <- function(x) {
-  library(tidyverse)
-  a = x %>%
-    filter(is.na(PublicationSubtype)|PublicationSubtype %in% c("editorialMaterial", "exhibitionCatalogue", "letter", "meetingAbstract", "newsItem", "publishedPaper"))
-    
+
+#filter_content_ovr_vet_no: tar bort kategori "Övrigt vetenskapligt"
+filter_content_ovr_vet_no <- function(divadata) {
+  a = divadata %>%
+    filter(ContentType != "Övrigt vetenskapligt")
   return(a)
 }
-csvall2 <- read_csv("/home/shub/assets/diva/diva_researchpubl_df_latest.csv")
-test <- filter_konf_publ(csvall2)
+
+
+# Status ------------------------------------------------------------------
+
+#filter_status_publ_yes: tar fram publicerat material
+filter_status_publ_yes <- function(divadata) {
+  a = divadata %>%
+    filter(is.na(Status)|Status == "published")
+  return(a)
+}
+
+#filter_status_publ_no: tar fram opublicerat material
+filter_status_publ_no <- function(divadata) {
+  a = divadata %>%
+    filter(Status %in% c("accepted", "aheadofprint", "inPress", "submitted"))
+  return(a)
+}
+
+
+# Year --------------------------------------------------------------------
+
+#filter_years: filtrerar på år
+filter_years <- function(divadata, startyear, endyear) {
+  a = divadata %>%
+    filter(Year >= startyear & Year <= endyear)
+}
+
+# FullTextLink ------------------------------------------------------------
+
+#filter_fulltextlink_yes: tar fram publ med fulltextlänk
+filter_fulltextlink_yes <- function(divadata) {
+  a = divadata %>%
+    filter(!is.na(FullTextLink))
+  return(a)
+}
+
+
+# PublicationSubType -----------------------------------------------------
+
+#filter_publsubtype_papers: tar bort de kategorier som inte är papers (abstracts, poster, presentation)
+filter_publsubtype_papers <- function(divadata) {
+  a = divadata %>%
+    filter(is.na(PublicationSubtype)|PublicationSubtype %in% c("editorialMaterial", "exhibitionCatalogue",
+                                                               "letter", "meetingAbstract", "newsItem",
+                                                               "publishedPaper"))
+  
+  return(a)
+}
+
+
+# Reviewed ----------------------------------------------------------------
+
+
+#filter_reviewed_yes: tar fram granskade publikationer
+filter_reviewed_yes <- function(divadata) {
+  a = divadata %>%
+    filter(Reviewed == "true")
+  return(a)
+}
+
+#filter__reviewed_no: tar fram ogranskade publikationer
+filter_reviewed_no <- function(divadata) {
+  a = divadata %>%
+    filter(Reviewed == "false")
+  return(a)
+}

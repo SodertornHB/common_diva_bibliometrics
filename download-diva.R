6fa41b1074e3edc26f255a5f958a2edf84dbc54e
Camilla Hertil Lindelöw <camilla.hertil.lindelow@sh.se>
2017-09-20 13:19:57 +0000

    anpasssat till Tidyverse.

--- download-diva.R
+++ download-diva.R
@@ -1,3 +1,16 @@
+#
+#
+#
+#download-diva
+#170920 CHL (senaste uppdatering:anpassad till Tidyverse)
+#laddar ner DiVA-data (2 csv-filer) via DiVAs API en gång per dygn. Sparar en kopia bakåt i tiden. 
+#csv-filerna delas upp och kombineras till 5 bastibbles: forskningspublikationer och forskare med och
+#utan Sh-affiliering samt studentuppsatser.
+#
+#
+#
+
+library(tidyverse)
 
 #
 # Filnamnet vi vill att nedladdade filer ska ha, %format% byts ut mot det specifika formatet, ex.v.
@@ -5,11 +18,14 @@
 #
 # %timestamp% byts ut mot datum och tid som filen laddades ner.
 #
+#
 
+#lokal filväg
 filename = "/home/shub/assets/diva/diva_%format%_%timestamp%.csv"
 
 dir.create(dirname(filename), showWarnings = FALSE)
 
+#max 50000 rader, kan komma att behöva uppdateras framöver
 origins = list("csvall2_allt" = "http://sh.diva-portal.org/smash/export.jsf?format=csvall2&noOfRows=500000",
                "csv2_allt" = "http://sh.diva-portal.org/smash/export.jsf?format=csv02&noOfRows=500000"
                )
@@ -21,7 +37,7 @@ for (format in names(origins)) {
   # Vi laddar ner respektive en gång om dagen, så kolla att senaste versionen är tillräckligt gammal.
   # Efter att vi laddat ner en ny fil så uppdaterar vi länken med namn "latest", ex.v.:
   #
-  # diva-csvall-latest.csv
+  # diva_csvall2_allt_latest.csv
   #
   # så att denna i stället pekar på den nyss nedladdade filen.
   #
@@ -37,7 +53,7 @@ for (format in names(origins)) {
   }
   #
   # Efter att ha laddat ner och uppdaterat länkar så tar vi bort eventuella tidigare nedladdningar.
-  # Vi behåller dock två kopior bakåt i tiden.
+  # Vi behåller dock en kopia bakåt i tiden.
   #
   l = list.files(path = dirname(cfile), pattern = sub("%timestamp%", ".*", basename(f)))
   if (length(l) >= 4) {
@@ -50,61 +66,68 @@ for (format in names(origins)) {
 
 #------------------------------------------------------------------------------------------------------------------------
 #
-#bearbetning av nedladdad data: förbered dataframess att spara till csv-filer.
+#bearbetning av nedladdad data: förbered tibbles.
 #
   
-#Nr 1: författarfraktionerad df utan studentuppsatser
-csvall2_df <- read.csv("/home/shub/assets/diva/diva_csvall2_allt_latest.csv",
-                       header=TRUE,
-                       sep=",",
-                       encoding="UTF-8",
-                       na.strings=c("","NA"),
-                       stringsAsFactors = FALSE
-)
+#Nr 1: författarfraktionerad tibble utan studentuppsatser
+csvall2 <- read_csv("/home/shub/assets/diva/diva_csvall2_allt_latest.csv", col_names = TRUE)
+#PID blir felaktigt format vid inläsning, därför:
+colnames(csvall2)[1] <- "PID"
 
-csv2_df <- read.csv("/home/shub/assets/diva/diva_csv2_allt_latest.csv",
-                 header=TRUE,
-                 sep=",",
-                 encoding="UTF-8",
-                 na.strings=c("","NA"),
-                 stringsAsFactors = FALSE
-)
 
-author_df <- merge(x = csv2_df, y = csvall2_df, by.x = "PID", by.y = "PID", all.x = TRUE)
+csv2 <- read_csv("/home/shub/assets/diva/diva_csv2_allt_latest.csv", col_names = TRUE)
+#PID blir felaktigt format vid inläsning, därför:
+colnames(csv2)[1] <- "PID"
 
-#sortera bort studentuppsatser
-author_df <- author_df[!(author_df$PublicationType == "Studentuppsats" |
-                           author_df$PublicationType == "Studentuppsats/Examensarbete" |
-                           author_df$PublicationType == "Studentuppsats (Examensarbete)"),]
+
+author <- left_join(csv2, csvall2, by = "PID")
+
+#sortera bort studentuppsatser ur författar-tibble
+author <- filter(author, !(PublicationType %in% c("Studentuppsats",
+                                                  "Studentuppsats/Examensarbete",
+                                                  "Studentuppsats (Examensarbete)"
+                                                  )
+                           )
+                 )
+
 #eller: 
-#author_df <- author_df[!(is.na(author_df$ContentType)),]
-#vilken är mest tillförlitlig? PublicationType (säger Greta)
+#author <- author[!(is.na(author$ContentType)),] syntax för read.csv
+#vilken är mest tillförlitlig? PublicationType säger Greta
 
-#Nr 2: dela csvall2_allt i två delar, en med uppsatser och en utan (resultat: 2 df)
-studentessays_df <- csvall2_df[(csvall2_df$PublicationType == "Studentuppsats" |
-                                csvall2_df$PublicationType == "Studentuppsats/Examensarbete" |
-                                csvall2_df$PublicationType == "Studentuppsats (Examensarbete)"),]
+#Nr 2: dela csvall2 i två delar, en med uppsatser och en utan (resultat: 2 tibbles). Notera utropstecknet.
+studentessays <- filter(csvall2, (PublicationType %in% c("Studentuppsats", 
+                                                        "Studentuppsats/Examensarbete", 
+                                                        "Studentuppsats (Examensarbete)"
+                                                        )
+                                  )
+                        )
 
-researchpubl_df <- csvall2_df[!(csvall2_df$PublicationType == "Studentuppsats" |
-                                  csvall2_df$PublicationType == "Studentuppsats/Examensarbete" |
-                                  csvall2_df$PublicationType == "Studentuppsats (Examensarbete)"),]
+researchpubl <- filter(csvall2, !(PublicationType %in% c("Studentuppsats", 
+                                                               "Studentuppsats/Examensarbete", 
+                                                               "Studentuppsats (Examensarbete)"
+                                                        )
+                                 )
+                       )
 
-#Nr 3: dra endast Sh ur researchpubl och author (resultat: 2 df)
-author_sh_df <- author_df[!(is.na(author_df$OrganisationIds)),]
-researchpubl_sh_df <- researchpubl_df[(grepl("\\[481\\]", researchpubl_df$Name)),]
+
+#Nr 3: endast Sh-affilierade publikationer från researchpubl och author (resultat: 2 tibbles)
+author_sh <- filter(author, !(is.na(OrganisationIds)))
+researchpubl_sh <- filter(researchpubl, (grepl("\\[481\\]", Name)))
 
 
-list_of_dataframes <- list("author_df" = author_df, "studentessays_df" = studentessays_df, "researchpubl_df" = researchpubl_df,
-                           "author_sh_df" = author_sh_df, "researchpubl_sh_df" = researchpubl_sh_df)
+#-----------------------------------------------------------------------------------------------------------------
+#spara undan de 5 tibbles
+list_of_tibbles <- list("author" = author, "studentessays" = studentessays, "researchpubl" = researchpubl,
+                           "author_sh" = author_sh, "researchpubl_sh" = researchpubl_sh)
 
-for (df in names(list_of_dataframes)) {
-  af = sub("%format%", df, filename)
+for (t in names(list_of_tibbles)) {
+  af = sub("%format%", t, filename)
   
   afile = sub("%timestamp%", "latest", af)
   if(is.na(file.info(afile)$mtime) ||
      file.info(afile)$mtime < Sys.time()-(60*60*24)) {
     as = sub("%timestamp%", format(Sys.time(), "%Y%m%d_%H%M"), af)
-    write.csv(list_of_dataframes[[df]], as)
+    write.csv(list_of_tibbles[[t]], as)
     if (file.exists(afile)) {
       file.remove(afile)
     }
@@ -113,7 +136,7 @@ for (df in names(list_of_dataframes)) {
   
   #
   # Efter att ha laddat ner och uppdaterat länkar så tar vi bort eventuella tidigare nedladdningar.
-  # Vi behåller dock två kopior bakåt i tiden.
+  # Vi behåller dock en kopia bakåt i tiden.
   #
   l = list.files(path = dirname(afile), pattern = sub("%timestamp%", ".*", basename(af)))
   if (length(l) >= 4) {
@@ -122,3 +145,4 @@ for (df in names(list_of_dataframes)) {
     }
   }
 }
+

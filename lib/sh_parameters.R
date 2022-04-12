cbb43d2419655185ab5443f307eea7880ce58780
GretaL <greta.linder@sh.se>
2022-04-12 07:28:03 +0000

    Uppdaterad org

--- lib/sh_parameters.R
+++ lib/sh_parameters.R
@@ -36,6 +36,7 @@ genusvetenskap <- c("7390", "7391", "7392", "7393", "11814")
 geografi <- c("7394", "7395", "11833")
 historia <- c("7399", "7400", "7401", "7402", "11803")
 idehistoria <- c("7353", "7354", "7355", "7356", "11807")
+informatik <- c("7408", "886705")
 hs <- c("11802", "485", "497", "7356", "7381", "7402", "7409", "7462", "7485", "7499", "7500", "7501", 
         "11803", "11804", "11805", "11806", "11807", "11808", "11846", "884751")
 ikl <- c("11809", "495", "498", "5550", "7374", "7378", "7384", "7393", "7456", "7460", "7479", "7489",
@@ -47,18 +48,19 @@ internationella_relationer <- c("11824")
 journalistik <- c("7403", "7405", "7406", "7407", "11825")
 juridik <-  c("11827", "883451")
 konstvetenskap <- c("7455", "7456", "11815")
-lararutbildning <- c("495", "11821", "873601")
+lararutbildning <- c("495", "11821", "873601", "886703")
+lararinst <- c("873601", "886701", "886702", "886703", "886704")
 litteraturvetenskap <- c("7457", "7458", "7459", "7460",  "11816")
 maltidskunskap <- c("7473", "11838")
 maris <- c("7462", "11845")
-matematisk_didaktik <- c("15800")
+matematisk_didaktik <- c("15800", "886704")
 medieteknik <- c("7465", "7466", "7467", "7468", "11835")
 miljovetenskap <- c("7469", "7470", "11837")
 mkv <- c("498", "7351", "7352", "11817")
 nationalekonomi <- c("7474", "7475", "7476", "7477", "11826")
 nmt <- c("11831", "484", "486", "7362", "7395", "7408", "7454", "7467", "7468", "7470", "7472", "7473",
          "7521", "7522", "7523", "7527", "11751", "11831", "11832", "11833", "11834", "11835", "11837",
-         "11838", "11839", "11840", "15800")
+         "11838", "11839", "11840", "15800", "886700", "886705")
 offent_forv <- ("884753")
 pedagogik <- c("7478", "7479", "11818")
 polis <- c("873602", "884752")
@@ -78,6 +80,7 @@ shb <- c("2050")
 socialt_arbete <- c("9000", "11829")
 sociologi <- c("7505", "7506", "7507", "7508", "11841")
 statsvetenskap <- c("7513", "7514", "7515", "7516", "11830")
+sustainlab <- c("886700")
 svenska <- c("7517", "7518", "7519", "7520", "11820")
 turismvetenskap <- c("7521", "7522", "7523", "11839")
 utveckling_och_int_samarbete <- c("7527", "11840")
@@ -85,15 +88,16 @@ utveckling_och_int_samarbete <- c("7527", "11840")
 #medelsfördelningsspecifika parametrar:
 
 #för kopplingar institution/ämne:
-inst_list <- list("hs" = hs, "ikl" = ikl, "nmt" = nmt, "sam" = sam, "polis" = polis)
-#centra: maris, scohost, cbees, ims, reinvent, politikens_org
+inst_list <- list("hs" = hs, "ikl" = ikl, "nmt" = nmt, "sam" = sam,
+                  "polis" = polis, "lararinst" = lararinst)
+#centra: maris, scohost, cbees, ims, reinvent, politikens_org, sustainlab
 centra_list <- c("enter" = enter, "maris" = maris, "scohost" = scohost, "ims" = ims, "reinvent" = reinvent,
-                 "politikens_org" = politikens_org)
+                 "politikens_org" = politikens_org, "sustainlab" = sustainlab)
 #polis samma som institution
 ämnen_list <- c("arkeologi" = arkeologi, "arkivvetenskap" = arkivvetenskap, "biologi" = biologi, "engelska" = engelska,
                 "estetik" = estetik, "etnologi" = etnologi, "foretagsekonomi" = foretagsekonomi, "filosofi" = filosofi,
                 "genusvetenskap" = genusvetenskap, "geografi" = geografi, "historia" = historia,
-                "idehistoria" = idehistoria, "internationell_halsa" = internationell_halsa,
+                "idehistoria" = idehistoria, "informatik" = informatik, "internationell_halsa" = internationell_halsa,
                 "internationella_relationer" = internationella_relationer, "journalistik" = journalistik, "juridik" = juridik,
                 "konstvetenskap" = konstvetenskap, "litteraturvetenskap" = litteraturvetenskap,
                 "lararutbildning" = lararutbildning, "maltidskunskap" = maltidskunskap,"matematisk_didaktik" = matematisk_didaktik,
